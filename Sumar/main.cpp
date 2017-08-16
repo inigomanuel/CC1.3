ca743f3ae5e382419039f1fb2e0cd1428ee99b1c
ALUMNO-D <ALUMNO-D@UCSP.AULAB>
2017-08-16 09:19:14 -0500

    Sumar dos variables

---
+++ Sumar/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y,z;
+    cout << "Ingrese el primer numero: " << endl;
+    cin >> x;
+    cout << "Ingrese el segundo numero: " << endl;
+    cin >> y;
+    z=x+y;
+    cout << "Resultado Final: "<< z << endl;
+    return 0;
+}
