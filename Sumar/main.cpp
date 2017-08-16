4eb0552295daff12ee21e6502df5dd0aa25074d0
ALUMNO-D <ALUMNO-D@UCSP.AULAB>
2017-08-16 09:41:37 -0500

    Sumar varios numeros

--- Sumar/main.cpp
+++ Sumar/main.cpp
@@ -4,12 +4,16 @@ using namespace std;
 
 int main()
 {
-    int x,y,z;
+    int x,y,z,p,c;
     cout << "Ingrese el primer numero: " << endl;
     cin >> x;
     cout << "Ingrese el segundo numero: " << endl;
     cin >> y;
     z=x+y;
     cout << "Resultado Final: "<< z << endl;
+    cout << "Ingrese el segundo numero: " << endl;
+    cin >> p;
+    c=p+z;
+    cout << "Resultado Final: "<< c << endl;
     return 0;
 }
