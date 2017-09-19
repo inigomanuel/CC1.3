1c0ba78c2b8bf562ac1e1713f7da4d20a8de64a3
ALUMNO-D <ALUMNO-D@UCSP.AULAB>
2017-09-19 09:58:28 -0500

    Control Practica

--- Control/main.cpp
+++ Control/main.cpp
@@ -2,31 +2,32 @@
 #include <string.h>
 using namespace std;
 
-/*
-int invertir_cadena_recursiva(int arr[],int longitud){
-    if()
-        return longitud-1;
-    else
-        return arr[longitud-longitud-1];
+
+int invertir_cadena_recursiva(int arr[],int comienzo, int fin){
+    int temp;
+    if (comienzo < fin){
+        temp=arr[comienzo];
+        arr[comienzo]=arr[fin];
+        arr[fin]=temp;
+
+        return invertir_cadena_recursiva(arr,comienzo+1,fin-1);
+    }
 }
-*/
 
 int main()
 {
     //1
-    /*
     int tamanio1=4,tamanio2=6;
     int matriz[4][6]={{1,2,3,4,5,6},
                         {7,8,9,10,11,12},
                         {13,14,15,16,17,18},
                         {19,20,21,22,23,24}};
-    for(int i=0;i<tamanio1;i++)
+    for(int i=0;i<tamanio1;i++){
         for(int j=0;j<tamanio2;j++){
-            cout<<matriz[i][j]<<" ";
+            cout<<matriz[i][j];
         }
-    */
+    }
     //2
-    /*
     int c=5;
     int cadena[5]={1,2,3,4,5};
     int temp;
@@ -38,21 +39,27 @@ int main()
     for(int j=0;j<c;j++){
         cout<<cadena[j];
     }
-    */
     //3
-    /*
     int longitud=5;
     int arr[5]={1,2,3,4,5};
-    invertir_cadena_recursiva(arr,longitud);
-    */
+    invertir_cadena_recursiva(arr,0,longitud-1);
+    for(int i=0;i<longitud;i++){
+        cout<<arr[i]<<" ";
+    }
     //4
+    int x;
     char str1[40]="hola";
     char str2[40]="voy a terminar mis tareas";
     char str3[40]="cerrare el face";
     char str4[40]="bye";
     char str5[40];
-
-
+    char str6[40];
+    for (x=0; x<40; x++){
+        str5[x] = str1[x];
+    }
+    for (x=0; x<40; x++){
+        str6[x] = str2[x];
+    }
     cout<<str1<<endl;
     cout<<"      "<<str2<<endl;
     cout<<"      "<<str3<<endl;
@@ -60,8 +67,9 @@ int main()
     cout<<" "<<endl;
     cout<<strcpy(str1,str4)<<endl;
     cout<<"      "<<strcpy(str2,str3)<<endl;
-    cout<<"      "<<strcpy(str3,str2)<<endl;
-    cout<<strcpy(str5,str1)<<endl;
+    cout<<"      "<<str6<<endl;
+    cout<<str5<<endl;
+
     return 0;
 }
 
@@ -76,8 +84,6 @@ int main()
 
 salida
 1 2 3 4 5 6 12 18 24 23 22 21 20 19 13 7 8 9 10 11 17 16 15 14
-
-3)hacer el ejercicio 2 pero con recursividad
 
 4)implemente un programa que lea por teclado un numero de cadenas y las imprima en orden inverso usando recursividad
 
